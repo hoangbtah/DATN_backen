1f9bf372602306ab253fdc308e790746687e1a0a
Bui Viet Hoang <hoangbui.1808@gmail.com>
2024-01-19 22:54:14 +0700

    backen

---
+++ web_AMIS/web_AMIS/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_AMIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hoangbv");
+        }
+        [HttpPost]
+        public int Post()
+        {
+            return 1;
+        }
+    }
+}
---
+++ web_AMIS/web_AMIS/Controllers/EmptyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_AMIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmptyController : ControllerBase
+    {
+
+    }
+}
