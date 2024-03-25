ac0f74cfba1fa32e047e6aaebc7d61f5d3b4a2bb
Bui Viet Hoang <hoangbui.1808@gmail.com>
2024-03-25 22:55:36 +0700

    25/03

--- web_AMIS/MISA_WEBHAUI_AMIS/Entities/Manufactorer.cs
+++ web_AMIS/MISA_WEBHAUI_AMIS/Entities/Manufactorer.cs
@@ -9,7 +9,7 @@ namespace MISA_WEBHAUI_AMIS_Core.Entities
     public class Manufactorer
     {
         public Guid ManufactorerId { get; set; }
-        public string Name { get; set; }
+        public string ManufactorerName { get; set; }
         public string Address { get; set; }
         
     }
--- web_AMIS/MISA_WEBHAUI_Api/obj/Debug/net6.0/MISA_WEBHAUI_Api.AssemblyInfo.cs
+++ web_AMIS/MISA_WEBHAUI_Api/obj/Debug/net6.0/MISA_WEBHAUI_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MISA_WEBHAUI_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ca5d2c0b2b74c2e49b8922366b2cae943e489b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2dc711915eba88745beab8de85495e112b675f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MISA_WEBHAUI_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MISA_WEBHAUI_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
